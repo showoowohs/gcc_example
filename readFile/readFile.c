06c38b5c0f1e2cb88a877b2512d8b4a011bdb26f
Benson Lin <showoowohs@gmail.com>
2016-07-04 19:13:45 +0800

    可以寫 file

--- readFile/readFile.c
+++ readFile/readFile.c
@@ -31,58 +31,14 @@ char* load_file(char const* path)
 	return buffer;
 }
 
-char* load_file2(char const* path)
-{
-	char* buffer = 0;
-	long length;
-	FILE * f = fopen (path, "rb"); //was "rb"
-
-	if (f)
-	{
-		fseek (f, 0, SEEK_END);
-		length = ftell (f);
-		fseek (f, 0, SEEK_SET);
-		buffer = (char*)malloc ((length+1)*sizeof(char));
-		if (buffer)
-		{
-			fread (buffer, sizeof(char), length, f);
-		}
-		fclose (f);
-	}
-	buffer[length+1] = '\0';
-	int i;
-	// for (i = 0; i < length; i++) {
-	//     printf("buffer[%d] == %c\n", i, buffer[i]);
-	// }
-	//printf("buffer = %s\n", buffer);
-
-	return buffer;
-}
-
-char *string_concat(char *str1, char *str2) {  
-	// 計算所需的陣列長度  
-	int length=strlen(str1)+strlen(str2)+1;  
-
-	// 產生新的陣列空間  
-	char *result = (char*)malloc(sizeof(char) * length);  
-
-	// 複製第一個字串至新的陣列空間  
-	strcpy(result, str1);  
-	// 串接第二個字串至新的陣列空間  
-	strcat(result, str2);  
-
-	return result;  
-}  
-
 int main(int argc, char *argv[]) {
 
+	// 1. read andia_app.mk to tmp
 	char * tmp = load_file("./andia_app.mk");
-	//strcpy(tmp, "# Live Wallpapers");
-	//string_concat(tmp,"# Live Wallpapers");
 
 	//printf("buffer = %s\n", tmp);
 	
-	//load_file2("./andia_app.mk");
+	// 2. read vcm30t30.mk to line 
 	FILE * fp;
 	char * line = NULL;
 	size_t len = 0;
@@ -102,10 +58,22 @@ int main(int argc, char *argv[]) {
 			//printf("%s\n", line);
 		}
 
-			printf("%s", line);
+		printf("%s", line);
+
 	}
 
 	fclose(fp);
+
+	// 3. write line connect to output.txt
+
+	FILE *fptr;
+	fptr=fopen("program.txt","w");
+	if(fptr==NULL){
+		printf("Error!");
+		exit(1);
+	}
+	fprintf(fptr,"%s",line);
+	fclose(fptr);
 
 	return 0;
 }
