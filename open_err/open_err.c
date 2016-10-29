cd212066cae0e8dc22ab3d2e6a282d48990ba87c
benson <benson.ph.lin@gmail.com>
2016-10-29 14:53:07 +0800

    透過此實驗可知道 1. 我們可以透過 perror() 印出錯誤訊息 2. 可透過strerror() 印出錯誤的結果

--- open_err/open_err.c
+++ open_err/open_err.c
@@ -6,13 +6,29 @@
 
 int main(int argc, char *argv[])
 {
+#define USE_PERROR
+//#define USE_STRERROR
+#ifdef USE_PERROR
+	void perror(const char *msg);
+	int fd;  
+	fd=open("/dev/tty_hci", O_RDWR);  
+	if(fd==-1)  
+	{  
+		// if opne is fail, will print a message
+		perror("open fail\n");  
+		exit(EXIT_FAILURE);  
+	}  
+#endif
+#ifdef USE_STRERROR
 	char *strerror(int errnum);
 	int fd;  
 	fd=open("/dev/tty_hci", O_RDWR);  
 	if(fd==-1)  
 	{  
-		perror("open\n");  
+		printf( "Error Value is : %s\n", strerror(errno) );
 		exit(EXIT_FAILURE);  
-	}  
+	}else // if open file success, will return a message 
+		printf( "Error Value is : %s\n", strerror(errno) );
+#endif
 }
 
